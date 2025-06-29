b498f4f86884a65b53ac82e27807bab628c4264d
Gabriel Arpini <arpinigabrielsr71@gmail.com>
2025-06-29 15:47:44 -0300

    try some new stuff in cnn and start new implementation in trainbook

--- src/cnn.py
+++ src/cnn.py
@@ -9,9 +9,11 @@ class NeuralNet(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
         self.conv3 = nn.Conv2d(64, 32, kernel_size=3, padding=1, stride=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(p=0.5)
         self.fc1 = nn.Linear(32 * 32 * 32, 128)
         self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84,10)
+        
         
     def forward(self, x):
         x = F.relu(self.conv1(x))  # -> (32, 256, 256)
@@ -21,8 +23,8 @@ class NeuralNet(nn.Module):
         x = F.relu(self.conv3(x))  # -> (32, 64, 64)
         x = self.pool(x)  # -> (32, 32, 32)
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
         x = self.fc3(x)
         
         return x
