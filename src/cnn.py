6efc2731e91daf7e4b8067c7b956f8ee06594377
Gabriel Arpini <arpinigabrielsr71@gmail.com>
2025-08-08 11:58:48 -0300

    recreate equivariant cnn

--- src/cnn.py
+++ src/cnn.py
@@ -1,7 +1,136 @@
-from torch import nn
+#from torch import nn
 import torch.nn.functional as F
 import torch
+from escnn import gspaces
+from escnn import nn
 
+class NeuralNet(nn.Module):
+    """Equivariant CNN implementation."""
+    def __init__(self, num_classes=10):
+        super(NeuralNet, self).__init__()
+
+        # Create Group action
+        r2_act = gspaces.rot2dOnR2(N=8) # 8 rotations
+
+        # Create input type
+        in_type = nn.FieldType(self.r2_act, 3*[self.r2_act.trivial_repr])
+        self.input_type = in_type
+        
+        # Create conv1 
+        out_type = nn.FieldType(self.r2_act, 32*[self.r2_act.trivial_repr])
+        self.block1 = nn.SequentialModule(
+            nn.MaskModule(in_type, 29, margin=1),
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # Create conv2 
+        # Uses the old out_type as in_type for this
+        in_type = self.block1.out_type
+        out_type = nn.FieldType(self.r2_act, 64*[self.r2_act.trivial_repr])
+        self.block2 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+        # Create first pooling layer
+
+        self.pool1 = nn.SequentialModule(
+            nn.PointWiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        # Conv3
+        in_type = self.block2.out_type
+        out_type = nn.FieldType(self.r2_act, 128*[self.r2_act.trivial_repr]) 
+        self.block3 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # Conv4
+        in_type = self.block3.out_type
+        out_type = nn.FieldType(self.r2_act, 256*[self.r2_act.trivial_repr])
+        self.block4 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+        self.pool2 = nn.SequentialModule(
+            nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        # Conv5
+        in_type = self.block4.out_type
+        out_type = nn.FieldType(self.r2_act, 512*[self.r2_act.trivial_repr])
+        self.block5 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # Conv6 
+        in_type = self.block5.out_type
+        out_type = nn.FieldType(self.r2_act, 256*[self.r2_act.trivial_repr])
+        self.block6 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=3, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        ) 
+        self.pool3 = nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=1, padding=0)
+        
+        self.gpool = nn.GroupPooling(out_type)
+        
+        # number of output channels
+        c = self.gpool.out_type.size
+        
+        # Fully Connected
+        self.fully_net = torch.nn.Sequential(
+            torch.nn.Linear(c, 256),
+            torch.nn.BatchNorm1d(256),
+            torch.nn.ELU(inplace=True),
+            torch.nn.Linear(128, n_classes),
+        )
+
+        def forward(self, input: torch.Tensor):
+        # wrap the input tensor in a GeometricTensor
+        # (associate it with the input type)
+        x = nn.GeometricTensor(input, self.input_type)
+        
+        # apply each equivariant block
+        
+        # Each layer has an input and an output type
+        # A layer takes a GeometricTensor in input.
+        # This tensor needs to be associated with the same representation of the layer's input type
+        #
+        # The Layer outputs a new GeometricTensor, associated with the layer's output type.
+        # As a result, consecutive layers need to have matching input/output types
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.pool1(x)
+        
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.pool2(x)
+        
+        x = self.block5(x)
+        x = self.block6(x)
+        
+        # pool over the spatial dimensions
+        x = self.pool3(x)
+        
+        # pool over the group
+        x = self.gpool(x)
+
+        # unwrap the output GeometricTensor
+        # (take the Pytorch tensor and discard the associated representation)
+        x = x.tensor
+        
+        # classify with the final fully connected layers)
+        x = self.fully_net(x.reshape(x.shape[0], -1))
+
+        return x
 class NeuralNet(nn.Module):
     """ Convolutional Neural Network arquitecture. """
     def __init__(self):
